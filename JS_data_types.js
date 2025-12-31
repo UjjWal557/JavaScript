fcb2c9b0d44ba8980774a4a34ae238dd753c7495
Ujjwal <ujjwal777roy@gmailcom>
2025-12-31 21:44:06 +0530

    Data Types

---
+++ JS_data_types.js
@@ -0,0 +1,34 @@
+//String
+let str="hello";
+console.log(str);//hello
+console.log(typeof str); //string, 'typeof' is used to identify the data type of a variable
+
+//Number
+let num=45.67;
+console.log(num); //45.67
+console.log(typeof num); //number   
+
+//Boolean
+let bool=true;
+console.log(bool); //true
+console.log(typeof bool); //boolean
+
+//Undefined
+let undef;
+console.log(undef); //undefined
+console.log(typeof undef); //undefined
+
+//Null      
+let n=null;
+console.log(n); //null
+console.log(typeof n); //object
+
+//Array
+let arr=[1, "text", true, null];
+console.log(arr); //[ 1, 'text', true, null ]
+console.log(typeof arr); //object (arrays are a type of object in JavaScript)
+
+//Object
+let obj={name: "John", age: 30};
+console.log(obj); //{ name: 'John', age: 30 }
+console.log(typeof obj); //object
